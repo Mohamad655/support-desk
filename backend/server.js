9c2252a871edd9b17ab0a29f8219f03f230abfb5
Mohammad <md.haq7892@gmail.com>
2022-07-28 01:19:11 +0530

    created backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Server....");
+const express = require("express");
+const color = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 8000;
+
+//connect to Database
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
