50c053ea180246744f58c3653989784e21af5de6
Mohammad <md.haq7892@gmail.com>
2022-08-01 17:09:21 +0530

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const color = require("colors");
 const dotenv = require("dotenv").config();
@@ -11,11 +12,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.send("HELLO");
-});
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcone to Support Desk API" });
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
